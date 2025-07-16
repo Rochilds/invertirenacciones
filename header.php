68f92de897133c49fd6fcc6c08f963d22fa33c00
Luis Rocha <luisalbertorocha73@gmail.com>
2025-07-16 16:24:59 -0300

    se agrega dominio en head

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <meta name="facebook-domain-verification" content="uktr8jy9n0wjkledxwvc2cxft4ozh2" />
 
   <!-- Título dinámico -->
   <title><?php wp_title( '|', true, 'right' ); ?></title>
