27835078aba18295d66f73b598a4b5f16cdd5c2d
Luis Rocha <luisalbertorocha73@gmail.com>
2025-07-15 22:41:04 -0300

    se modifica a php

--- functions.php
+++ functions.php
@@ -4,7 +4,18 @@
  */
 
 function invertir_en_acciones_assets() {
-    // Encolar style.css del tema
+    // Encolar style.css del tema 
+
+    add_action( 'wp_enqueue_scripts', function() {
+    echo "\n<!-- ENCOLANDO STYLE.CSS -->\n";
+    wp_enqueue_style(
+        'invertir-en-acciones-style',
+        get_stylesheet_uri(),
+        [],
+        wp_get_theme()->get('Version')
+    );
+}, 20 );
+
     wp_enqueue_style(
         'invertir-en-acciones-style',   // Handle único
         get_stylesheet_uri(),           // URL a style.css en la raíz del tema
