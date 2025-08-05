6eb1bcf75dd69baa4e3cecf9127b0ad4b48b300f
Luis Rocha <luisalbertorocha73@gmail.com>
2025-08-05 07:37:40 -0300

    se agrega pixel de nuevo

--- header.php
+++ header.php
@@ -8,7 +8,15 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
-  <meta name="facebook-domain-verification" content="uktr8jy9n0wjkledxwvc2cxft4ozh2" />
+  <meta name="facebook-domain-verification" content="uktr8jy9n0wjkledxwvc2cxft4ozh2" /> 
+
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KHMBXGHN');</script>
+<!-- End Google Tag Manager -->
 
   <!-- Título dinámico -->
   <title><?php wp_title( '|', true, 'right' ); ?></title>
@@ -111,23 +119,7 @@
 </script>
 
 
-<!-- Meta Pixel Code -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '722266103917354'); 
-  fbq('track', 'PageView');
-</script>
-<noscript>
-  <img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=722266103917354&ev=PageView&noscript=1"/>
-</noscript>
+
 
 
 
@@ -138,5 +130,10 @@
  <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?>> 
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHMBXGHN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
